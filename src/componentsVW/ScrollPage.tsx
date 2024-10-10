c8876a96f3efee9c942b8cc1b513987b559a7099
Vucko <vuckosm91@gmail.com>
2024-10-10 19:56:25 +0200

    build: Animations for the main Pages

--- src/componentsVW/ScrollPage.tsx
+++ src/componentsVW/ScrollPage.tsx
@@ -15,6 +15,10 @@ gsap.registerPlugin(ScrollTrigger);
 const ScrollPage = () => {
   const pageOneRef = useRef(null);
   const pageTwoRef = useRef(null);
+  const pageThreeRef = useRef(null);
+  const pageFourRef = useRef(null);
+  const coverImageRef = useRef(null);
+  const pageFiveRef = useRef(null);
 
   const mediaBreakPoint = window.matchMedia("(min-width: 1024px)");
 
@@ -28,12 +32,14 @@ const ScrollPage = () => {
         x: 0,
         scrollTrigger: {
           trigger: pageOneRef.current,
-          start: "top top",
-          end: "bottom 5%",
+          start: "50% top",
+          end: "bottom top",
           scrub: true,
         },
       }
     );
+
+    /* Second Page Animate */
 
     gsap.fromTo(
       pageTwoRef.current,
@@ -44,7 +50,143 @@ const ScrollPage = () => {
         x: 0,
         scrollTrigger: {
           trigger: pageTwoRef.current,
-          start: "top 75%",
+          start: "top 100%",
+          end: "bottom bottom",
+          scrub: true,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      pageTwoRef.current,
+      { opacity: 1, y: 0, x: 0 },
+      {
+        opacity: 0,
+        y: 0,
+        x: 0,
+        scrollTrigger: {
+          trigger: pageTwoRef.current,
+          start: "50% top",
+          end: "bottom top",
+          scrub: true,
+        },
+      }
+    );
+
+    /* Third Page Animate */
+
+    gsap.fromTo(
+      pageThreeRef.current,
+      { opacity: 0, y: 0, x: 0 },
+      {
+        opacity: 1,
+        y: 0,
+        x: 0,
+        scrollTrigger: {
+          trigger: pageThreeRef.current,
+          start: "top 100%",
+          end: "bottom bottom",
+          scrub: true,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      pageThreeRef.current,
+      { opacity: 1, y: 0, x: 0 },
+      {
+        opacity: 0,
+        y: 0,
+        x: 0,
+        scrollTrigger: {
+          trigger: pageThreeRef.current,
+          start: "50% top",
+          end: "bottom top",
+          scrub: true,
+        },
+      }
+    );
+
+    /* Fourth Page Animate */
+
+    gsap.fromTo(
+      pageFourRef.current,
+      { opacity: 0, y: 0, x: 0 },
+      {
+        opacity: 1,
+        y: 0,
+        x: 0,
+        scrollTrigger: {
+          trigger: pageFourRef.current,
+          start: "top 100%",
+          end: "bottom bottom",
+          scrub: true,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      pageFourRef.current,
+      { opacity: 1, y: 0, x: 0 },
+      {
+        opacity: 0,
+        y: 0,
+        x: 0,
+        scrollTrigger: {
+          trigger: pageFourRef.current,
+          start: "50% top",
+          end: "bottom top",
+          scrub: true,
+        },
+      }
+    );
+
+    /* Cover Image Animate */
+
+    gsap.fromTo(
+      coverImageRef.current,
+      { opacity: 0, y: 0, x: 0 },
+      {
+        opacity: 1,
+        y: 0,
+        x: 0,
+        scrollTrigger: {
+          trigger: coverImageRef.current,
+          start: "top 100%",
+          end: "bottom bottom",
+          scrub: true,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      coverImageRef.current,
+      { opacity: 1, y: 0, x: 0 },
+      {
+        opacity: 0,
+        y: 0,
+        x: 0,
+        scrollTrigger: {
+          trigger: coverImageRef.current,
+          start: "50% top",
+          end: "bottom top",
+          scrub: true,
+        },
+      }
+    );
+
+    /* Fifth Page Animate */
+
+    gsap.fromTo(
+      pageFiveRef.current,
+      { opacity: 0, y: 0, x: 0 },
+      {
+        opacity: 1,
+        y: 0,
+        x: 0,
+        scrollTrigger: {
+          trigger: pageFiveRef.current,
+          start: "top 100%",
           end: "bottom bottom",
           scrub: true,
         },
@@ -54,30 +196,30 @@ const ScrollPage = () => {
 
   return (
     <ScrollContainer>
-      <div>
+      <div ref={pageOneRef}>
         <LandingPage />
       </div>
-      <div>
+      <div ref={pageTwoRef}>
         <AboutFirst />
       </div>
-      <div>
+      <div ref={pageThreeRef}>
         <AboutSecond />
       </div>
       {mediaBreakPoint.matches ? (
         <>
-          <div>
+          <div ref={coverImageRef}>
             <ProjectCoverL />
           </div>
-          <div>
+          <div ref={pageFourRef}>
             <ProjectCoverDescr />
           </div>
         </>
       ) : (
-        <div>
+        <div ref={pageFourRef}>
           <ProjectCoverSm />
         </div>
       )}
-      <div>
+      <div ref={pageFiveRef}>
         <ContactForm />
       </div>
     </ScrollContainer>
